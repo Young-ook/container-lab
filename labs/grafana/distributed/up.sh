f9b35b335b0e41e06c1f7994306cd14baca3236e
Young-ook <youngook.kim@gmail.com>
2025-08-31 21:20:10 +0900

    feat(lgtm): purge namespaces

--- labs/grafana/distributed/up.sh
+++ labs/grafana/distributed/up.sh
@@ -20,6 +20,22 @@ function helmrepo() {
 
 function setup() {
   echo "Installing applications"
+
+  ### creds
+  for ns in alloy loki mimir monitoring tempo; do
+    kubectl create ns $ns
+  done
+
+  htpasswd -cb .htpasswd $TENANT_ID $TENANT_PW
+  kubectl -n loki create secret generic basic-auth --from-file=.htpasswd
+  kubectl -n mimir create secret generic basic-auth --from-file=.htpasswd
+#  kubectl -n tempo create secret generic basic-auth --from-file=.htpasswd
+  rm .htpasswd
+
+  kubectl -n alloy create secret generic basic-auth \
+      --from-literal=username=$TENANT_ID \
+      --from-literal=password=$TENANT_PW
+
   ### apps
   helm upgrade --install -n loki loki grafana/loki \
       -f $VALS_DIR/loki.yaml --version $LOKI_CHART_VER \
@@ -29,23 +45,12 @@ function setup() {
       --create-namespace
 #  helm upgrade --install -n tempo tempo grafana/tempo-distributed -f $VALS_DIR/tempo.yaml --version ${TEMPO_CHART_VER} --create-namespace
 
-  htpasswd -cb .htpasswd $TENANT_ID $TENANT_PW
-  kubectl -n loki create secret generic basic-auth --from-file=.htpasswd
-  kubectl -n mimir create secret generic basic-auth --from-file=.htpasswd
-#  kubectl -n tempo create secret generic basic-auth --from-file=.htpasswd
-  rm .htpasswd
-
-  kubectl create ns alloy
-  kubectl -n alloy create secret generic basic-auth \
-      --from-literal=username=$TENANT_ID \
-      --from-literal=password=$TENANT_PW
-
-  helm upgrade --install -n alloy k8s-monitoring grafana/k8s-monitoring \
-      -f $VALS_DIR/alloy.yaml --version $K8SMON_CHART_VER
-
   helm upgrade --install -n monitoring grafana grafana/grafana \
       -f $VALS_DIR/grafana.yaml --version $GRAFANA_CHART_VER \
       --create-namespace
+
+  helm upgrade --install -n alloy k8s-monitoring grafana/k8s-monitoring \
+      -f $VALS_DIR/alloy.yaml --version $K8SMON_CHART_VER
 }
 
 # main
