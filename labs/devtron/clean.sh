d3340551f4373f6c165674bfc064ecc30cd067ff
Young-ook <youngook.kim@gmail.com>
2025-12-25 15:10:54 +0900

    fix(devtron): updated helmctl in setup/cleanup

--- labs/devtron/clean.sh
+++ labs/devtron/clean.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-helm uninstall -n devtroncd devtron
+set -e
 
-kubectl delete ns argo devtron-ci devtron-cd devtron-demo devtroncd
+VALS_DIR='./config'
+
+function progress() {
+  echo "+----------------------------------------------+"
+  echo " $1 "
+  echo "+----------------------------------------------+"
+}
+
+function uninstall() {
+  ### apps
+  progress "Uninstalling Devtron"
+  bash ../../scripts/helmctl "uninstall" "./release/devtron.yaml"
+
+  kubectl delete ns argo devtron-ci devtron-cd devtron-demo devtroncd
+}
+
+### main
+uninstall
+
